097c1f421e0326aea0e7a66f951b7d7e471995f9
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-01-09 17:27:42 +0200

    Repository cleanup

--- ap_application.cpp
+++ ap_application.cpp
@@ -1,9 +1,6 @@
 #include "ap_application.hpp"
 
 #include "graphics/gs_lvgl_service.hpp"
-
-#include "utils/SimpleSignal.hpp"
-
 #include "logger/logger_service.hpp"
 
 #include <optional>
@@ -28,6 +25,5 @@ Application::runApplicationLoop()
     while (true)
     {
         m_graphicsService->executeGlTask();
-        Simple::Lib::ExecuteLaterPool::Instance().processQueue();
     }
 }
